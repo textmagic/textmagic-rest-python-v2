c70707b9a75605606c7a2069b5ce9af4533c1093
Andrey Vorobyev <vorobivka@mail.ru>
2019-11-01 10:24:13 +0000

    Fix default scheme for SDK

--- TextMagic/configuration.py
+++ TextMagic/configuration.py
@@ -40,7 +40,7 @@ class Configuration(object):
             return
 
         # Default Base url
-        self.host = "http://rest.textmagic.com"
+        self.host = "https://rest.textmagic.com"
         # Temp file folder for downloading files
         self.temp_folder_path = None
 
@@ -240,5 +240,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2\n"\
-               "SDK Package Version: 2.0.826".\
+               "SDK Package Version: 2.0.865".\
                format(env=sys.platform, pyversion=sys.version)
